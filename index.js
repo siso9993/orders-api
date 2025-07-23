a8e025839427755c9cbdaaccea1de0488e325a48
siso9993 <siso9993@gmail.com>
2025-07-23 11:50:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function normalize(r) {
     dovod_storna:                    clean(r['OBJ.RefVPrDovodstorn']),
     cislo_faktury:                   clean(r['OBJ.VPrCislofakturyt']),
     objednane_u_dodavatela:          clean(r['OBJ.VPrObjednaneUdod']),
-    planovane_naskladnenie_na_nas_sklad:          clean(r['OBJ.VPrDatDodKuNam']),
+    planovane_odhadovane_naskladnenie_na_nas_sklad:          clean(r['OBJ.VPrDatDodKuNam']),
     predpokladany_datum_dodania_zakaznikovi:     clean(r['OBJ.VPrDatumDodPom']),
     dovod_meskania:                  clean(r['OBJ.VPrDovodMeskanTo'])
   };
